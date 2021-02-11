d063d5313974c42ad8fb90a1fdc9c55cfce17823
il7605 <77802449+il7605@users.noreply.github.com>
2021-02-11 21:55:43 +0300

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("GeekBrains2")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("GeekBrains2")]
+[assembly: AssemblyTitle("GeekBrains2")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GeekBrains2.Program
+// Assembly: GeekBrains2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: F58098E4-1AE6-4EE8-A9F5-7C11FD117350
+// Assembly location: C:\GeekBrains2\GeekBrains2\GeekBrains2\bin\Debug\netcoreapp3.1\GeekBrains2.dll
+
+using System;
+
+namespace GeekBrains2
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.Write("Введите число ");
+      if ((Convert.ToInt32(Console.ReadLine()) & 1) == 0)
+        Console.WriteLine("Четное число");
+      else
+        Console.WriteLine("Нечетное число");
+      Console.ReadLine();
+    }
+  }
+}
